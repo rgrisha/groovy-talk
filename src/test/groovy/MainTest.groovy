4c73f76b8a4f9211b58c621cc232732c11e2bf2b
rgrisha <rgrisha@yahoo.com>
2016-10-04 14:02:45 +0300

    test engine now Spock

--- src/test/groovy/MainTest.groovy
+++ src/test/groovy/MainTest.groovy
@@ -1,31 +1,35 @@
-import org.junit.Assert
-import org.testng.annotations.Test
 
 /**
  * Created by rolandas on 10/4/16.
  */
 
 
-@Test
-class MainTest {
+class MainTest extends spock.lang.Specification {
 
-  public void testCellEquality() {
-    Assert.assertEquals(new Cell(1,1), new Cell(1,1))
+  def "two different instances of cell with same coords are equal"() {
+    def cell1 = new Cell(1,1)
+    def cell2 = new Cell(1,1)
+
+    expect:
+      cell1 == cell2
   }
 
-  public void testCellInequality() {
-    Assert.assertNotEquals(new Cell(2,1), new Cell(1,1))
+  def "two cells with different coords are not equal"() {
+    def cell1 = new Cell(2,1)
+    def cell2 = new Cell(1,1)
+
+    expect:
+    !cell1.equals(cell2)
   }
 
-  public void testRuleDefinitionWorks() {
-
+  def "rule description gives correct rule"() {
     def rules = RulesDsl.make {
       when live with { neighbours == 2 } then live
     }
 
-    Assert.assertEquals(rules[0].cellKind, "live");
-    Assert.assertEquals(rules[0].action, "live");
-
+    expect:
+      rules[0].cellKind == "live"
+      rules[0].action == "live"
   }
 
 }
